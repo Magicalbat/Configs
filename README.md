d2ec8d45078d05ce78e4218c08d6e4d5064eee1c
Magicalbat <magicalbat@comcast.net>
2022-12-11 15:31:28 -0500

    Add nvim configs

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Configs
+
+A repository to store my config files.
+
+## Neovim
+I set up neovim following [this](https://www.youtube.com/watch?v=ctH-a-1eUME&list=PLhoH5vyxr6Qq41NFL4GvhFp-WLd5xzIzZ) tutorial series.
---
+++ nvim/lua/user/options.lua
@@ -0,0 +1,46 @@
+local options = {
+    backup = false,
+    clipboard = "unnamedplus",
+    cmdheight = 2,
+    completeopt = { "menuone", "noselect" } ,
+    conceallevel = 0,
+    fileencoding = "utf-8",
+    hlsearch = true,
+    ignorecase = true,
+    mouse = "a",
+    pumheight = 10,
+    showmode = false,
+    showtabline = 2,
+    smartcase = true,
+    smartindent = true,
+    splitbelow = true,
+    splitright = true,
+    swapfile = false,
+    termguicolors = true,
+    timeoutlen = 1000,
+    undofile = true,
+    updatetime = 300,
+    writebackup = false,
+    expandtab = true,
+    shiftwidth = 4,
+    tabstop = 4,
+    cursorline = false,
+    number = true,
+    relativenumber = false,
+    numberwidth = 4,
+    signcolumn = "yes",
+    wrap = false,
+    scrolloff = 4,
+    sidescrolloff = 4,
+    guifont = "Cascadia Mono:h10",
+    --guifont = "monospace:h11",
+}
+
+for k, v in pairs(options) do
+    vim.opt[k] = v
+end
+
+vim.cmd("autocmd BufEnter * set formatoptions-=cro")
+vim.cmd("autocmd BufEnter * setlocal formatoptions-=cro")
+
+vim.opt.shortmess:append "c"
